9099a09c66c2ffcd640ddb859120cc7a18b8bd42
most21 <most1@jhu.edu>
2020-05-29 09:40:46 -0400

    fixed bug with comment display and add has_many/belongs_to relationship for users/comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :article
+  belongs_to :user
 
-  scope :visible_comments, ->(user) { where(is_public: true).or(where("user_id = ?", user.email)) }
+  scope :visible_comments, ->(user) { where(is_public: true).or(where(user_id: user.id)) }
 end
