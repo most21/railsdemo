a5de79e1968e1ac73b4fd64f5f4d550a67cb5dd9
most21 <most1@jhu.edu>
2020-06-02 15:59:12 -0400

    added search functionality

--- app/javascript/components/ArticleList.js
+++ app/javascript/components/ArticleList.js
@@ -3,11 +3,39 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 class ArticleList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+    };
+
+    this.searchInput = React.createRef();
+    this.updateSearchTerm = this.updateSearchTerm.bind(this);
+    //this.matchSearchTerm = this.matchSearchTerm.bind(this);
+  } // end constructor
+
+  updateSearchTerm() {
+    this.setState({ searchTerm: this.searchInput.current.value });
+  } // end updateSearchTerm
+
+  matchSearchTerm(obj) {
+    const {
+      id, created_at, updated_at, comments, user_id, ...rest
+    } = obj;
+    const { searchTerm } = this.state;
+    console.log(searchTerm)
+    return Object.values(rest).some(
+      value => value.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1,
+    );
+  } // end matchSearchTerm
+
   renderArticles() {
     const { activeId, articles } = this.props;
-    articles.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+    const filteredArticles = articles
+      .filter(el => this.matchSearchTerm(el))
+      .sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
 
-    return articles.map(article => (
+    return filteredArticles.map(article => (
       <li key={article.id}>
         <Link to={`/articles/${article.id}`} className={activeId === article.id ? 'active' : ''}>
           {article.created_at}
@@ -16,7 +44,7 @@ class ArticleList extends React.Component {
         </Link>
       </li>
     ));
-  }
+  } // end renderArticles
 
   render() {
     return (
@@ -25,11 +53,21 @@ class ArticleList extends React.Component {
           Articles
           <Link to="/articles/new">New Article</Link>
         </h2>
+
+        <input
+          className="search"
+          placeholder="Search"
+          type="text"
+          ref={this.searchInput}
+          onKeyUp={this.updateSearchTerm}
+        />
+
         <ul>{this.renderArticles()}</ul>
       </section>
     );
-  }
-}
+  } // end render
+
+} // end ArticleList
 
 ArticleList.propTypes = {
   activeId: PropTypes.number,
